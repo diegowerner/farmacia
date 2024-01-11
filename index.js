af7ec63274d5d437f1d452ae9dd11aebe619c445
Diego Werner <diegowerner__@hotmail.com>
2024-01-11 16:56:23 -0300

    a

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.CONNECTIONSTRING)
     console.log('Conectado no BD');
     app.emit('pronto');
 })
-.catch (e => console.log(e));
+.catch (e => console.log(e)); 
 
 
 
