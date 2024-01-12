a066e0d2a4014482b47bc12bc52b330b990a8344
Diego Fernando Werner <81124652+diegowerner@users.noreply.github.com>
2024-01-11 23:52:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 mongoose.set('strictQuery', false);
 const connectDB = async () => {
@@ -47,7 +47,7 @@ app.use(routes);
 
 
 connectDB().then(() => {
-    app.listen(port, "0.0.0.0", () => {
+    app.listen(PORT, "0.0.0.0", () => {
         console.log(`Conectado na porta ${PORT}`);
     })
 })
